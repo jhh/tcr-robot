3342a2799b784786f57bd5d9556b06743a13b67b
Jeff Hutchison <jeff@jeffhutchison.com>
2022-03-18 10:23:18 -0400

    Update for 2022 dependencies

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,9 +8,12 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.NetworkButton;
 import frc.robot.commands.TalonRunCommand;
 import frc.robot.subsystems.TalonSubsystem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.strykeforce.telemetry.TelemetryController;
 import org.strykeforce.telemetry.TelemetryService;
 
@@ -21,6 +24,7 @@ import org.strykeforce.telemetry.TelemetryService;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
+    private final static Logger logger = LoggerFactory.getLogger(RobotContainer.class);
     // The robot's subsystems and commands are defined here...
     private final TelemetryService telemetryService = new TelemetryService(TelemetryController::new);
     private final TalonSubsystem talonSubsystem = new TalonSubsystem(telemetryService);
@@ -44,6 +48,19 @@ public class RobotContainer {
     private void configureButtonBindings() {
         var entry = NetworkTableInstance.getDefault().getEntry("Trigger");
         new NetworkButton(entry).whenPressed(new TalonRunCommand(talonSubsystem));
+
+        entry = NetworkTableInstance.getDefault().getEntry("TestTrigger");
+        new NetworkButton(entry).whenPressed(new InstantCommand() {
+            @Override
+            public void initialize() {
+                logger.info("TestTrigger whenPressed");
+            }
+
+            @Override
+            public boolean runsWhenDisabled() {
+                return true;
+            }
+        });
     }
 
     /**
